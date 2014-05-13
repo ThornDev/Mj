2c4d368ce58251566699a7798b2ad22d51364dbd
qingyun <qingyun@jack-wang.local>
2014-05-13 10:16:46 +0800

    Initial Commit

---
+++ GCDSample/MjViewController.h
@@ -0,0 +1,13 @@
+//
+//  MjViewController.h
+//  GCDSample
+//
+//  Created by qingyun on 14-5-13.
+//  Copyright (c) 2014年 hnqingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MjViewController : UIViewController
+
+@end
---
+++ GCDSample/MjViewController.m
@@ -0,0 +1,29 @@
+//
+//  MjViewController.m
+//  GCDSample
+//
+//  Created by qingyun on 14-5-13.
+//  Copyright (c) 2014年 hnqingyun. All rights reserved.
+//
+
+#import "MjViewController.h"
+
+@interface MjViewController ()
+
+@end
+
+@implementation MjViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
